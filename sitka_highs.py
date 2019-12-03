faafed20c901b0a200696cba38fdb5549084e9f4
Stephen Joseph Samuels <stevesam1927@gmail.com>
2019-12-03 19:21:58 +0530

    DOWNLOADING DATA - Printing the Headers and Their Positions

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,6 +4,7 @@ filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
+    
     # print(header_row)
 
     for index, column_header in enumerate(header_row):
