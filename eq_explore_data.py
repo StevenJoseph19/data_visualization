b7b004a4ed9a9f7a48a0a8616bb456019beee002
Stephen Joseph Samuels <stevesam1927@gmail.com>
2019-12-05 11:50:06 +0530

    Extracting Magnitudes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,8 +5,10 @@ filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
     all_eq_dicts = all_eq_data['features']
-    # readable_file = 'data/readable_eq_data.json'
     print(len(all_eq_dicts))
 
-# with open(readable_file, 'w') as f:
-#     json.dump(all_eq_data, f, indent=4)
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+print(mags[:10])
